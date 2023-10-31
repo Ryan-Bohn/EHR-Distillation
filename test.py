022b8d59dfac3b44f89043aca3c581d3beae0d78
delphynium <dlphynm@gmail.com>
2023-10-31 03:57:47 -0700

    fix cuda issues on ihm

---
+++ test.py
@@ -0,0 +1,10 @@
+import torch
+print(torch.cuda.is_available())
+print(torch.version.cuda)
+print(torch.cuda.device_count())
+print(torch.cuda.get_device_name(0))
+print(torch.cuda.current_device())
+tensor = torch.randn(3, 3)
+tensor = tensor.to('cuda')
+print(tensor)
+print("Hello World")
